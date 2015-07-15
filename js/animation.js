fe3ccfc2aa9c8047ff6e943cde4f7136b04212a8
Luke Engelhardt <luke@securitymetrics.com>
2015-07-15 13:54:59 -0600

    saving work

--- js/animation.js
+++ js/animation.js
@@ -6,16 +6,22 @@ $(document).ready(function() {
       $('#tweet-controls').show();
         $(this).css('height', '200%');
         });
+
     var counter = $('#char-count').html();
 
 
     $('.tweet-compose').keydown(function()  {
-        $('#char-count').html(counter -= 1);
+        $('#char-count').html(counter = 140 - $('textarea').val().length);
         if (counter <= 10) {
-            $('#char-count').css('color', 'red')
+            $('#char-count').css('color', 'red');
+        } else {
+            $('#char-count').css('color', '#999');
         }
         if (counter <= 0) {
-            $('.button').attr('disabled', 'disabled');
+            $('.button').attr('disabled', true);
+            $('#char-count').html(counter = 0);
+        } else if (counter > 0) {
+            $('.button').removeAttr('disabled');
         }
     });
 
@@ -25,10 +31,9 @@ $(document).ready(function() {
         var newTweet = '<div class="tweet">' +
             '<div class="content">' +
             '<img class="avatar" src="img/alagoon.jpg">' +
-            '<strong class="fullname">Testy McTesterson  </strong>' +
+            '<strong class="fullname">Franky Noprep</strong>' +
             '<span class="username">@15minlecturesFTL</span>' +
             '<p class ="tweet-text">' + newTweetText + '</p>' +
-                // '<p class="tweet-text">this text literally shows up</p>'
             '</div></div>';
 
         return newTweet;
@@ -38,9 +43,19 @@ $(document).ready(function() {
         $('#stream').prepend(addNewTweet);
     });
 
+    $('.tweet-actions').hide();
 
+    $('.tweet').hover(function() {
+        $('.tweet-actions', this).show();
+    }, function() {
+        $('.tweet-actions', this).hide();
+    });
 
+    $('.stats').hide();
 
+    $('.tweet').click(function() {
+        $('.stats', this).slideToggle('fast');
+        })
 
 
 });
